49ed6fc93acd3e4d5271bea2bdd0dfd941b2ed98
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-08-21 02:23:19 +0300

    commit tree 6e57e98d77648f62eff46a4a6362beaec439f45d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    arr.select { |str| @word.chars.sort == str.chars.sort }
+  end
+end
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana]) 
